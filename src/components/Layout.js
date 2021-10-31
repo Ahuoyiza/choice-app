1ba21b87ba6dc68337c8282ab52eec85060fc2e7
sophia <sophieoyiza@gmail.com>
2021-10-31 12:56:56 +0100

    set up

---
+++ src/components/Layout.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import{ 
+    Flex,
+    Box,
+    Text,
+    Heading,
+    Image,
+  } from '@chakra-ui/react';
+
+const Layout = () => {
+    return (
+        <Flex>
+            <Heading>Layout</Heading>
+        </Flex>
+    )
+}
+
+export default Layout
